5508112dd92874168dabe8a279ad21e4b0f10e0c
Gerson Cruz <gcruz1776@gmail.com>
2019-05-13 22:22:24 -0500

    created pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "./utils/API";
-import Card from "./components/Card"
+import Card from "./components/dares/Card"
 
 
 import './App.css';
@@ -19,7 +19,8 @@ class App extends Component {
   loadDares = () => {
     API.getDares()
       .then(res =>
-        this.setState({ dares: res.data, type: "", author: "", details: "", id: "" })
+        this.setState({ dares: res.data, type: "", author: "", details: "", id: "" }
+        )
       )
       .catch(err => console.log(err));
   };
@@ -28,16 +29,20 @@ class App extends Component {
 
   render() {
     return (
-      < div className = "App" >        
-      {this.state.dares.map(dare => {
-        <Card
-        type={dare.type}
-        author={dare.author}
-        details={dare.details}
-        id={dare.id}/>
-      })}
-    </div >
-    )}
+      < div className="App" >
+      <h1>hi</h1>
+        {this.state.dares.map((dare, i) => (
+          <Card
+            key={i}
+            type={dare.type}
+            author={dare.author}
+            details={dare.details}
+            id={dare.id}
+          />
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/components/dares/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Card(props) {
+
+  return (
+    //   creating basic layout of quote card
+    <div className="card">
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Type:</strong> {props.type}
+          </li>
+          <li>
+            <strong>Author:</strong> {props.author}
+          </li>
+          <li>
+            <strong>Details:</strong> {props.details}
+          </li>
+          <li>
+            <strong>Id:</strong> {props.id}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
